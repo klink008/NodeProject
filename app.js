5d01e8ac47ce6d6579ba69e4a60fba291cfaac08
Robert Lazarony <lazarony.3@gmail.com>
2015-11-12 09:26:14 -0500

    adding mongoose

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'main/templates'));
@@ -25,6 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.listen('3000', function() {
+  console.log('I\'m listening...');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
