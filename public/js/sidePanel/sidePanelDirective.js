9277d0c846d0b9fe1996880c6dc4086407d79d46
Klink008 <Klink008@netjets.com>
2016-02-22 16:24:32 -0500

    Adding directive back to VCS.

---
+++ public/js/sidePanel/sidePanelDirective.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('sidePanel')
+        .directive('sidePanel', SidePanel);
+
+    function SidePanel(){
+        return {
+            restrict: 'E',
+            replace: 'true',
+            templateUrl: "templates/sidePanel.html",
+            controller: "sidePanelController",
+            controllerAs: "sidePanelCtrl"
+        }
+    }
+})();
